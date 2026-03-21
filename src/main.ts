c4207f9b25acda8c80ad6eeaacaacbc096646dca
Victor Raúl García García <victor07mejor@gmail.com>
2026-03-21 10:49:55 -0600

    feat: inicializa la documentacion de la API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mi súper API')
+    .setDescription('Documentación autogenerada de mi proyecto en NestJS')
+    .setVersion('1.0')
+    .build();
+
+  // Crear el documento y montar la ruta
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
