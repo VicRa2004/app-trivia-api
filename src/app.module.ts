1d5b0331d9ebfec60c39aad3d52f7ba7aa90d0df
Victor Raúl García García <victor07mejor@gmail.com>
2026-03-20 18:54:54 -0600

    feat: añade lo necesario para categgorias y auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { UsersModule } from './modules/users/users.module';
 import { QuizzesModule } from './modules/quizzes/quizzes.module';
 import { GameModule } from './modules/game/game.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, QuizzesModule, GameModule, PrismaModule],
+  imports: [UsersModule, QuizzesModule, GameModule, PrismaModule, CategoriesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
